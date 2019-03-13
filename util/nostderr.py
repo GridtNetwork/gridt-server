b192e6aae6f3049347d0c7a16626d7bc08dd7bdd
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2019-03-13 23:54:45 +0100

    Add a function to surpress stderr

---
+++ util/nostderr.py
@@ -0,0 +1,16 @@
+import contextlib
+import sys
+
+@contextlib.contextmanager
+def nostderr():
+    savestderr = sys.stderr
+    class Devnull(object):
+        def write(self, _): pass
+        def flush(self): pass
+    sys.stderr = Devnull()
+    try:
+        yield
+    finally:
+        sys.stderr = savestderr
+
+
