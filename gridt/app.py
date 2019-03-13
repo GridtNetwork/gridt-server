0d46395a52fd9d5ceb6fcead4ea7b8c4c0e092d2
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2019-03-13 14:17:24 +0100

    Change the way configuration of the server works

--- gridt/app.py
+++ gridt/app.py
@@ -10,9 +10,8 @@ test environment.
 
 import os
 import sys
+import pathlib
 import click
-
-sys.path.append("/home/robin/Documents/work/gridt/gridt-server/gridt/")
 
 from flask import Flask, jsonify
 from flask.cli import FlaskGroup
@@ -27,23 +26,36 @@ from resources.register import LoggedInResource, RegisterResource
 
 def create_app(overwrite_conf=None):
     """
-    :param overwrite_conf: default None, argument should be one of: development, testing, default.
+    :param overwrite_conf: default None, argument should be the name (excluding the .conf suffix) of the conf file in conf/ that you want to use.
+
+    Example: ::
+
+        gridt/
+            app.py
+            conf/
+                default.conf
+                your_file.conf
+
+    Then you can run ``create_app('your_file')`` to overwrite whatever is in the ``FLASK_CONFIGURATION`` bash variable in that moment.
+
     """
     app = Flask(__name__)
-    config = {
-        "development": "conf/development.cfg",
-        "testing": "conf/testing.cfg",
-        "default": "conf/default.cfg",
-    }
     config_name = os.getenv("FLASK_CONFIGURATION", "default")
 
     if overwrite_conf:
         config_name = overwrite_conf
 
-    app.logger.debug(f"Starting flask with {config_name} config.")
-    app.config.from_pyfile(config[config_name])
-    db.init_app(app)
+    try:
+        path = pathlib.Path('conf/') / (config_name + '.conf')
+        path = os.getcwd() / path
+        app.config.from_pyfile(str(path))
+    except FileNotFoundError:
+        app.logger.error(f"Could not find file conf/{config_name}.conf, exiting.")
+        sys.exit(1)
 
+    app.logger.debug(f"Starting flask with {config_name} config.")
+
+    db.init_app(app)
     JWT(app, authenticate, identify)
 
     api = Api(app)
@@ -52,8 +64,8 @@ def create_app(overwrite_conf=None):
 
     @app.cli.command()
     def initdb():
-        app.logger.debug(
-            f"Writing to database '{app.config['SQLALCHEMY_DATABASE_URI']}'"
+        app.logger.info(
+            f"Writing to database '{app.config['SQLALCHEMY_DATABASE_URI']}'."
         )
         db.create_all()
 
---
+++ gridt/tests/unit/test_app.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from unittest.mock import patch
+from flask import Config
+
+from app import create_app
+
+
+class AppTest(TestCase):
+    def test_overwrite_conf(self):
+        with patch.object(Config, "from_pyfile", return_value=None) as mocked_fun:
+            app = create_app("test_conf")
+            mocked_fun.assert_called_with('conf/test_conf.conf')
