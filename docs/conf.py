a692e7db510cf99646882fc1ccafddcbc156d7fa
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2019-03-18 21:37:26 +0100

    Restructure docs to prepare for the openapi part

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
+    "sphinxcontrib.openapi"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ docs/httpapireference.rst
@@ -0,0 +1,5 @@
+HTTP API Reference
+******************
+
+This document describes the HTTP API of the gridt server. For the python API of the server, see :doc:`pythonapireference`.
+
