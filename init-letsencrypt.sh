40ff90fddd26c8736e5cf7fcfe5f9c49f3cda2e5
Robin A. Dorstijn <robin@gridt.org>
2020-03-27 13:52:15 +0100

    Update email adress

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(api.gridt.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="robin@gridt.org" # Adding a valid address is strongly recommended
+email="info@gridt.org" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
