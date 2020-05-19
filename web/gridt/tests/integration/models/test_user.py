8db42c9fa909811ced9fa1cc37296c419327333d
PJvanVliet <p.j.van.vliet@student.rug.nl>
2020-05-19 20:43:34 +0200

    Moved tests from unit to integration

--- web/gridt/tests/integration/models/test_user.py
+++ web/gridt/tests/integration/models/test_user.py
@@ -22,3 +22,57 @@ class UserTest(BaseTest):
             user.delete_from_db()
 
             self.assertIsNone(User.query.filter_by(username="username").first())
+
+    def test_create(self):
+        with self.app_context(), nostderr():
+            user = User("name", "email", "password", role="role", bio="bio")
+            user.save_to_db()
+
+            self.assertIsNotNone(user.id)
+            self.assertEqual(user.username, "name")
+            self.assertEqual(user.email, "email")
+            self.assertEqual(user.role, "role")
+            self.assertEqual(user.bio, "bio")
+
+    def test_leaders(self):
+        with self.app_context():
+            user1 = User("user1", "test1@test.com", "test")
+            user2 = User("user2", "test2@test.com", "test")
+            user3 = User("user3", "test3@test.com", "test")
+
+            movement = Movement("movement1", "daily")
+
+            assoc1 = MovementUserAssociation(movement, user1, user2)
+            assoc2 = MovementUserAssociation(movement, user1, user3)
+
+            db.session.add_all(
+                [user1, user2, user3, assoc1, assoc2,]
+            )
+            db.session.commit()
+
+            self.assertEqual(len(user1.leaders(movement)), 2)
+            self.assertIn(user2, user1.leaders(movement))
+            self.assertIn(user3, user1.leaders(movement))
+    
+    def test_leaders_removed(self):
+        with self.app_context():
+            user1 = User("user1", "test1@test.com", "test")
+            user2 = User("user2", "test2@test.com", "test")
+            user3 = User("user3", "test3@test.com", "test")
+            user4 = User("user4", "test4@test.com", "test")
+
+            movement = Movement("movement1", "daily")
+
+            assoc1 = MovementUserAssociation(movement, user1, user2)
+            assoc2 = MovementUserAssociation(movement, user1, user3)
+            assoc3 = MovementUserAssociation(movement, user1, user4)
+            assoc3.destroy()
+
+            db.session.add_all(
+                [user1, user2, user3, user4, assoc1, assoc2, assoc3,]
+            )
+            db.session.commit()
+
+            self.assertEqual(len(user1.leaders(movement)), 2)
+            self.assertIn(user2, user1.leaders(movement))
+            self.assertIn(user3, user1.leaders(movement))
--- web/gridt/tests/unit/models/test_user.py
+++ web/gridt/tests/unit/models/test_user.py
@@ -24,58 +24,6 @@ class UserTest(BaseTest):
             self.assertEqual(user2.verify_password("password2"), True)
             self.assertEqual(user2.role, "administrator")
 
-            user3 = User("name", "email", "password", role="role", bio="bio")
-            user3.save_to_db()
-
-            self.assertIsNotNone(user3.id)
-            self.assertEqual(user3.username, "name")
-            self.assertEqual(user3.email, "email")
-            self.assertEqual(user3.role, "role")
-            self.assertEqual(user3.bio, "bio")
-
     def test_hash(self):
         user = User("username", "test@test.com", "test")
         self.assertTrue(user.verify_password("test"))
-
-    def test_leaders(self):
-        with self.app_context():
-            user1 = User("user1", "test1@test.com", "test")
-            user2 = User("user2", "test2@test.com", "test")
-            user3 = User("user3", "test3@test.com", "test")
-
-            movement = Movement("movement1", "daily")
-
-            assoc1 = MovementUserAssociation(movement, user1, user2)
-            assoc2 = MovementUserAssociation(movement, user1, user3)
-
-            db.session.add_all(
-                [user1, user2, user3, assoc1, assoc2,]
-            )
-            db.session.commit()
-
-            self.assertEqual(len(user1.leaders(movement)), 2)
-            self.assertIn(user2, user1.leaders(movement))
-            self.assertIn(user3, user1.leaders(movement))
-
-    def test_leaders_removed(self):
-        with self.app_context():
-            user1 = User("user1", "test1@test.com", "test")
-            user2 = User("user2", "test2@test.com", "test")
-            user3 = User("user3", "test3@test.com", "test")
-            user4 = User("user4", "test4@test.com", "test")
-
-            movement = Movement("movement1", "daily")
-
-            assoc1 = MovementUserAssociation(movement, user1, user2)
-            assoc2 = MovementUserAssociation(movement, user1, user3)
-            assoc3 = MovementUserAssociation(movement, user1, user4)
-            assoc3.destroy()
-
-            db.session.add_all(
-                [user1, user2, user3, user4, assoc1, assoc2, assoc3,]
-            )
-            db.session.commit()
-
-            self.assertEqual(len(user1.leaders(movement)), 2)
-            self.assertIn(user2, user1.leaders(movement))
-            self.assertIn(user3, user1.leaders(movement))
