319cc920484c3e7ab8d444a2c6fa5c424d6bb784
PJvanVliet <p.j.van.vliet@student.rug.nl>
2020-09-17 15:56:24 +0200

    Create dynamic templates for e-mail change

--- web/util/email_templates.py
+++ web/util/email_templates.py
@@ -10,10 +10,24 @@ def send_password_reset_email(email, token):
 
     send_email(email, template_id, template_data)
 
+
 def send_password_change_notification(email):
     template_id = current_app.config["PASSWORD_CHANGE_NOTIFICATION_TEMPLATE"]
+    template_data = {"link": "https://app.gridt.org/user/reset_password/request"}
+
+    send_email(email, template_id, template_data)
+
+def send_email_change_email(email, username, token):
+    template_id = current_app.config["EMAIL_CHANGE_TEMPLATE"]
     template_data = {
-        "link": "https://app.gridt.org/user/reset_password/request"
+        "username": username,
+        "link": f"https://app.gridt.org/user/change_password/confirm?token={token}"
     }
+
+    send_email(email, template_id, template_data)
+
+def send_email_change_notification(email, username):
+    template_id = current_app.config["EMAIL_CHANGE_NOTIFICATION_TEMPLATE"]
+    template_data = {"username": username}
 
     send_email(email, template_id, template_data)
