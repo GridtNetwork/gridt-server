7ca3ee178e502cd4e684526279c364d67ccfa867
PJvanVliet <p.j.van.vliet@student.rug.nl>
2020-09-17 18:13:13 +0200

    Update e-mail template

--- web/util/email_templates.py
+++ web/util/email_templates.py
@@ -21,7 +21,7 @@ def send_email_change_email(email, username, token):
     template_id = current_app.config["EMAIL_CHANGE_TEMPLATE"]
     template_data = {
         "username": username,
-        "link": f"https://app.gridt.org/user/change_password/confirm?token={token}"
+        "link": f"https://app.gridt.org/user/change_email/confirm?token={token}"
     }
 
     send_email(email, template_id, template_data)
