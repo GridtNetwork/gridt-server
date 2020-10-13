89a8296da09a6fffecd79952b107d471260ef039
R. Dorstijn <robbiedorstijn@gmail.com>
2020-10-13 15:44:28 +0000

    Make gridt point to secrets with kustomization

--- web/gridt/app.py
+++ web/gridt/app.py
@@ -14,6 +14,7 @@ import click
 from functools import reduce
 
 from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy
 
 from flask import Flask, jsonify
 from flask.cli import FlaskGroup
